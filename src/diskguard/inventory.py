a012906190767b019c6d7352137a77e47ddfd756
Dhanya16 <dhanyabailoor@gmail.com>
2026-07-25 17:13:30 +0530

    add inode usage and list n consumers

--- src/diskguard/inventory.py
+++ src/diskguard/inventory.py
@@ -1,20 +1,96 @@
+import os, shutil, socket
+from collections import defaultdict
 from datetime import datetime
-import shutil
-import socket
+
+TOP_N_CONSUMERS = 5
+
+EXCLUDED_DIRECTORIES = ["/dev", "/proc", "/sys", "/run"]
+
+def collect_filesystem_usage():
+    usage = shutil.disk_usage("/")
+    percent_used = (usage.used / usage.total) * 100
+
+    total = round(usage.total / (1024**3),2)
+    used = round(usage.used / (1024**3),2)
+    free = round(usage.free / (1024**3),2)
+    return {
+        "path": "/",
+        "total": total,
+        "used": used,
+        "free": free,
+        "percent": round(percent_used,2)
+    }
+
+def collect_inode_usage():
+    try:
+        fs_stats = os.statvfs("/")
+        if fs_stats is None:
+            raise Exception("Failed to get filesystem statistics")
+        total_inodes = fs_stats.f_files
+        used_inodes = total_inodes - fs_stats.f_ffree
+        free_inodes = fs_stats.f_ffree
+        percent_inodes_used = (used_inodes / total_inodes) * 100
+    except Exception as e:
+        return {
+            "available": False
+        }
+    return {
+        "available": True,
+        "total": total_inodes,
+        "used": used_inodes,
+        "free": free_inodes,
+        "percent": round(percent_inodes_used,2)
+    }
+
+def get_directory_size(directory):
+    # TODO: calculate directory size recursively
+    # returning dummy 3 for now
+    return 3
+
+def collect_top_n_consumers():
+    try:
+        consumers = defaultdict(int)
+        for directory in os.scandir("/"):
+            if directory.is_dir() and directory.path not in EXCLUDED_DIRECTORIES:
+                consumers[directory.name] = get_directory_size(directory.path)
+        consumers = sorted(consumers,reverse=True,key=lambda x: x[1])[:TOP_N_CONSUMERS]
+
+    except Exception as e:
+        return {
+            "available": False
+        }
+    return {
+        "available": True,
+        "consumers": consumers
+    }
 
 def collect_inventory():
     hostname = socket.gethostname()
     timestamp = datetime.now().astimezone()
-    usage = shutil.disk_usage("/")
-    percent_used = (usage.used / usage.total) * 100 
+
+    # filesystem usage
+    filesystem_usage = collect_filesystem_usage()
+
+    # inode usage: Filename ---> inode ---> actual data blocks
+    # An inode stores metadata such as:
+        # File permissions
+        # Owner
+        # Group
+        # File size
+        # Creation/modification times
+        # Pointers to file data blocks
+    inode_usage = collect_inode_usage()
+
+    # top N consumers
+    top_n_consumers = collect_top_n_consumers()
+
     return {
         "hostname": hostname,
         "timestamp": timestamp,
-        "filesystem": {
+        "filesystem": filesystem_usage,
+        "inode": {
             "path": "/",
-            "total": usage.total,
-            "used": usage.used,
-            "free": usage.free,
-            "percent": percent_used
-        }
+            "inode_usage": inode_usage
+        },
+        "top_n_consumers": top_n_consumers
 	}
--- src/diskguard/report.py
+++ src/diskguard/report.py
@@ -1,9 +1,25 @@
 def print_report(inventory,severity):
     print(f"Hostname: {inventory['hostname']}")
-    print(f"Timestamp: {inventory['timestamp']}\n")
-    print(f"Filesystem: {inventory['filesystem']['path']}")
-    print(f"Total: {inventory['filesystem']['total']}")
-    print(f"Used: {inventory['filesystem']['used']}")
-    print(f"Free: {inventory['filesystem']['free']}")
-    print(f"Percent: {inventory['filesystem']['percent']}\n")
-    print(f"Severity: {severity}")
+    print(f"Timestamp: {inventory['timestamp']}")
+    print("--------------------------------")
+    print(f"Filesystem Usage, path: {inventory['filesystem']['path']}\n")
+    print(f"Total    : {inventory['filesystem']['total']} GB")
+    print(f"Used     : {inventory['filesystem']['used']} GB")
+    print(f"Free     : {inventory['filesystem']['free']} GB")
+    print(f"Percent  : {inventory['filesystem']['percent']} %\n")
+    print("--------------------------------")
+    print(f"Inode Usage, path: {inventory['inode']['path']}\n")
+    if inventory['inode']['inode_usage']['available']:
+        print(f"Total    : {inventory['inode']['inode_usage']['total']} inodes")
+        print(f"Used     : {inventory['inode']['inode_usage']['used']} inodes")
+        print(f"Free     : {inventory['inode']['inode_usage']['free']} inodes")
+        print(f"Percent  : {inventory['inode']['inode_usage']['percent']} %\n")
+    else:
+        print("Inode usage unavailable\n")
+    print("--------------------------------")
+    print(f"Severity : {severity}\n") 
+    print("--------------------------------")
+    if inventory['top_n_consumers']['available']:
+        print(f"Top N Consumers: {inventory['top_n_consumers']['consumers']}\n")
+    else:
+        print("Top N Consumers unavailable\n")
